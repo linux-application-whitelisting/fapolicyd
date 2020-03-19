7dd1fdaf76718b0298f1aa04b3438528d46e9479
Steve Grubb <sgrubb@redhat.com>
2020-03-19 16:48:19 -0400

    Add option to cli to add files to the file trust database
    This is the first of 3 commits that will add the ability to manage the
    file backend database from the command line. This allows the admin to
    add a file to the database.

---
+++ src/library/file-backend.h
@@ -0,0 +1,32 @@
+/*
+ * file-backend.h - Header file for management functions of file-backend.c
+ * Copyright (c) 2020 Red Hat Inc.
+ * All Rights Reserved.
+ *
+ * This software may be freely redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor
+ * Boston, MA 02110-1335, USA.
+ *
+ * Authors:
+ *   Steve Grubb <sgrubb@redhat.com>
+ *   Radovan Sroka <rsroka@redhat.com>
+ */
+
+#ifndef FILE_BACKEND_HEADER
+#define FILE_BACKEND_HEADER
+
+int file_append(const char *path);
+
+#endif
+
