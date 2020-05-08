264cd055c7273afd88d3d1248342d75b0aa96d3e
Steve Grubb <sgrubb@redhat.com>
2020-05-08 17:01:51 -0400

    IMA check now implies doing the size check, too.

--- src/library/database.c
+++ src/library/database.c
@@ -831,7 +831,8 @@ retry_res:
 			if (retry == 0)
 				rc = get_ima_hash(fd, sha_xattr);
 			if (rc) {
-				if (strcmp(sha, sha_xattr)) {
+				if (size != info->size &&
+						strcmp(sha, sha_xattr)) {
 					if (retry == 0) {
 						retry = 1;
 						res = lt_read_db(path,
