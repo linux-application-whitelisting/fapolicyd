b1d13ef2130f7c9e834e7f65e088d65b540ed524
Ondrej Mosnacek <omosnace@redhat.com>
2026-03-19 10:31:15 +0100

    Add a CLI tool for measuring performance
    This takes the test-driver program from fapolicyd-extras [1] and
    integrates it into fapolicyd sources as a standalone fapolicyd-perf-test
    CLI tool. It can be used to measure performance of fapolicyd access
    decisions.
    
    Building the tool is disabled by default and can be enabled by passing
    --with-perf-test to ./configure. Distributions can decide if they want
    to ship the tool as a separate subpackage, or if they want to build it
    at all.
    
    Example usage:
    
        # touch file-list.txt
        # find /usr/lib64 -type f >>file-list.txt
        # find /usr/bin   -type f >>file-list.txt
        # find /usr/share -type f >>file-list.txt
        # wc -l file-list.txt
        15080 file-list.txt
        # fapolicyd-perf-test file-list.txt 2>/dev/null
        Starting scan...
        Elapsed: 0 seconds, 58 milliseconds
        #
    
    Running "perf record" from the test is not preserved, but similar result
    can be achieved by running the tool under perf:
    
        # perf record --call-graph dwarf fapolicyd-perf-test file-list.txt
        ...
        # perf script/report ...
    
    [1] https://github.com/linux-application-whitelisting/fapolicyd-extras/blob/main/performance/test-driver.c

---
+++ src/perf-test/fapolicyd-perf-test.c
@@ -0,0 +1,172 @@
+/*
+ * fapolicyd-perf-test.c - fapolicyd performance testing tool
+ * Copyright (c) 2025-2026 Red Hat Inc.
+ * All Rights Reserved.
+ *
+ * This software may be freely redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor
+ * Boston, MA 02110-1335, USA.
+ *
+ * Authors:
+ *   Steve Grubb <sgrubb@redhat.com>
+ *   Ondrej Mosnacek <omosnace@redhat.com>
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdatomic.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#include "config.h"
+
+#include "daemon-config.h"
+#include "message.h"
+#include "policy.h"
+#include "database.h"
+
+/* These need to be defined for the library */
+atomic_bool stop = 0;
+unsigned int debug_mode = 0;
+conf_t config = {};
+
+static char *get_line(FILE *f)
+{
+	char *line = NULL;
+	size_t len = 0;
+
+	while (getline(&line, &len, f) != -1) {
+		/* remove newline */
+		char *ptr = strchr(line, 0x0a);
+		if (ptr)
+			*ptr = 0;
+		return line;
+	}
+	free(line);
+	return NULL;
+}
+
+static int do_perf_test(FILE *input)
+{
+	int rc = 0, resp_fd;
+	pid_t our_pid;
+	struct timeval t0, t1;
+	char *path;
+
+	set_message_mode(MSG_STDERR, DBG_NO);
+	if (load_daemon_config(&config)) {
+		rc = 1;
+		goto out_reset_config;
+	}
+	if (load_rules(&config)) {
+		rc = 1;
+		goto out_reset_config;
+	}
+	// Setup lru caches
+	if (init_event_system(&config)) {
+		rc = 1;
+		goto out_rules;
+	}
+	if (init_database(&config)) {
+		rc = 1;
+		goto out_event_system;
+	}
+	// Init the file test libraries
+	file_init();
+	// Don't let it accidently emit audit events
+	policy_no_audit();
+
+	resp_fd = open("/dev/null", O_WRONLY|O_CLOEXEC);
+	if (resp_fd < 0) {
+		fprintf(stderr, "Can't open dev null\n");
+		rc = 1;
+		goto out_file;
+	}
+
+	our_pid = getpid();
+	printf("Starting scan...\n");
+
+	gettimeofday(&t0, NULL);
+	while ((path = get_line(input))) {
+		int fd = open(path, O_RDONLY|O_CLOEXEC);
+		free(path);
+		if (fd < 0)
+			continue;
+		// Build an "event" to exercise fapolicyd's decision making
+		struct fanotify_event_metadata metadata;
+		metadata.fd = fd; // listener closes after reply
+		metadata.pid = our_pid;
+		metadata.mask = FAN_OPEN_PERM;
+
+		make_policy_decision(&metadata, resp_fd, FAN_OPEN_PERM | FAN_OPEN_EXEC_PERM);
+	}
+	stop = 1;
+	gettimeofday(&t1, NULL);
+
+	long sec  = t1.tv_sec  - t0.tv_sec;
+	long usec = t1.tv_usec - t0.tv_usec;
+	if (usec < 0) {
+		usec += 1000000;
+		sec--;
+	}
+	long msec = usec / 1000;
+	printf("Elapsed: %ld seconds, %ld milliseconds\n", sec, msec);
+
+	close(resp_fd);
+out_file:
+	file_close();
+	close_database();
+out_event_system:
+	destroy_event_system();
+	unlink_fifo();
+out_rules:
+	destroy_rules();
+out_reset_config:
+	free_daemon_config(&config);
+	return rc;
+}
+
+static const char *USAGE =
+"Fapolicyd Performace Test\n\n"
+"Usage: %s [INPUT_FILE]\n\n"
+"Runs a dummy fapolicyd policy decision on each file from newline-separated\n"
+"list read from INPUT_FILE (or stdin if not specified) and prints the total"
+"time it took.\n"
+;
+
+int main(int argc, char * const argv[])
+{
+	FILE *input = stdin;
+	int rc;
+
+	if (argc > 2) {
+		printf(USAGE, argv[0]);
+		return 2;
+	}
+
+	if (argc > 1) {
+		input = fopen(argv[1], "r");
+		if (input == NULL) {
+			fprintf(stderr, "Error opening input file\n");
+			return 1;
+		}
+	}
+
+	rc = do_perf_test(input);
+	if (input != stdin)
+		fclose(input);
+	return rc;
+}
